f9d4e765dd9cc8824cf159de1b6ce4be3c4b8bec
gaotian <gt02115380@alibaba-inc.com>
2024-11-01 16:12:44 +0800

    feat:

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace ContosoPizza;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF { get; set; }
+
+    public string? Summary { get; set; }
+}
